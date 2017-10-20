638963856d9d2d9c40f76d674e30335a642636ec
Haggai Kaunda <haggaikaunda@berkeley.edu>
2017-10-20 06:07:31 +0000

    adding foreign_key sid to student_queue.

--- db/migrate/20171020045740_create_drop_in_histroys.rb
+++ db/migrate/20171020045740_create_drop_in_histroys.rb
@@ -1,9 +1,24 @@
 class CreateDropInHistroys < ActiveRecord::Migration
   def change
-    create_table :drop_in_histroys do |t|
-      t.string :student_id
 
-      t.timestamps null: false
+    create_table :drop_in_histroy do |t|
+      #entered by student, visible on tutor form
+      t.string :student_sid #should be foreign key.
+      t.string :course
+      t.integer :tutor_sid
+      t.time :start_time
+      t.time :end_time #endtime => create time since students are added to the histroy after being seen.
+      t.string :tutor_notes
+
+      #*** what will be the values of status? -- "in progress", "waiting" ---> just "no show"?
+      t.string :status
+      t.timestamps  #sort by create time.
+      
+      # t.timestamps null: false # not sure what 'null: false' means, maybe true?
+      
+      # t.time :end_time
+      # t.text :notes
+      # t.string :request_type //all students in the queue are here for dropin.
     end
   end
 end
--- db/migrate/20171020045803_create_student_queues.rb
+++ db/migrate/20171020045803_create_student_queues.rb
@@ -3,20 +3,16 @@ class CreateStudentQueues < ActiveRecord::Migration
 
     create_table :student_queue do |t|
       #entered by student, visible on tutor form
+      add_foreign_key :student_queue, :students, column: :student_sid primary_key: :sid
       t.string :student_sid #should be foreign key.
       t.string :course
       t.integer :tutor_sid
       t.time :start_time
 
-      #*** what will be the values of status? -- "in progress", "waiting" ---> just "no show"?
+      #*** what will be the values of status? -- "in progress", "waiting" 
       t.boolean :waiting?
       t.timestamps  #sort by create time.
       
-      # t.timestamps null: false # not sure what 'null: false' means, maybe true?
-      
-      # t.time :end_time
-      # t.text :notes
-      # t.string :request_type //all students in the queue are here for dropin.
     end
   end
 end
